595c51bb207555ad0b3126839724cc34e7e4553a
lucyandthelens <lucy@lucyandthelens.com>
2021-03-16 21:02:06 +0000

    Added Logo
    Added Image Logo

--- .eleventy.js
+++ .eleventy.js
@@ -68,6 +68,8 @@ module.exports = function(eleventyConfig) {
 
   eleventyConfig.addPassthroughCopy("img");
   eleventyConfig.addPassthroughCopy("css");
+  eleventyConfig.addPassthroughCopy("js")
+
 
   /* Markdown Overrides */
   let markdownLibrary = markdownIt({
