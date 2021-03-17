ae3901d7afb49eac6d0db0f3a56db6e07171df7d
lucyandthelens <lucy@lucyandthelens.com>
2021-03-17 18:17:29 +0000

    Moving Files
    Moving Files To Correct Path

--- .eleventy.js
+++ .eleventy.js
@@ -68,7 +68,7 @@ module.exports = function(eleventyConfig) {
 
   eleventyConfig.addPassthroughCopy("img");
   eleventyConfig.addPassthroughCopy("css");
-  eleventyConfig.addPassthroughCopy("js")
+  eleventyConfig.addPassthroughCopy("js");
 
 
   /* Markdown Overrides */
